41e4cdb2e2a41795cf4dec5c6433b760265b317a
s1xte3n <ntnhlapo16@gmail.com>
2025-05-30 10:20:42 +0200

    Teardown of Old Architecture

--- infrastructure/app.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import aws_cdk as cdk
-from my_resume_stack.my_resume_stack import ResumeStack
-
-app = cdk.App()
-ResumeStack(app, "ResumeStack")
-app.synth()
-# trigger CDK deploy
